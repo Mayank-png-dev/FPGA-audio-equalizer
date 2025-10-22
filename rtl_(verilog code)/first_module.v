46dc9e4b755632615b053a4b27fcece84408a8ba
Mayank-png-dev <msingh13_be23@thapar.edu>
2025-10-22 17:41:57 +0000

    written first few lines of code

---
+++ rtl_(verilog code)/first_module.v
@@ -0,0 +1,12 @@
+//fft_1024.v
+module fft_1024
+    input clk,
+    input rst,
+    input signed [15:0] data_in,
+    input data_valid,
+    output signed [15:0] data_out_real,
+    output signed [15:0] data_out_imag,
+    output out_valid
+);
+    // T0Do: Implement radix-2 fft
+endmodule
